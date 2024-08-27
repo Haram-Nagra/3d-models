7ddf0d7c9a20b1c174ca0d33b0ea8c47d9a16ad2
Haram Iqbal Nagra <hnagra.bscs21seecs@seecs.edu.pk>
2024-08-28 02:11:28 +0500

    made a full fledge earth

--- ball in ball.js
+++ ball in ball.js
@@ -25,10 +25,11 @@ controls.dampingFactor=0.03;
 
 
 
+
 // scene
 
 const scene =new THREE.Scene();
-const geo= new THREE.IcosahedronGeometry(1,2);
+const geo= new THREE.IcosahedronGeometry(1,3);
 const mat = new THREE.MeshStandardMaterial({
     color:0xffffff,
     flatShading:true
@@ -55,13 +56,24 @@ scene.add(wiremesh)
 mesh.add(wiremesh)
 // light
 
-const hemiLight =new THREE.HemisphereLight('red','white')
-scene.add(hemiLight)
+const hemiLight =new THREE.HemisphereLight('red','orange')
+// scene.add(hemiLight)
+
+const spotLight = new THREE.SpotLight(0xffffff,2,0,Math.PI/2,0,2.2)
+spotLight.position.set( 1,4,2 );
+spotLight.castShadow = true;
+spotLight.shadow.mapSize.width = 1024;
+spotLight.shadow.mapSize.height = 1024;
+spotLight.shadow.camera.near = 500;
+spotLight.shadow.camera.far = 4000;
+spotLight.shadow.camera.fov = 30;
+scene.add(spotLight)
 
 function animate(t=0){
     requestAnimationFrame(animate);
     mesh.rotation.y=t*0.001;
-    renderer.render(scene,camera)
+    renderer.render(scene,camera);
+    controls.update()
 }
 
 
