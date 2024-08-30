19a72d2ba899b531f332030fdca051aeffb608bc
Haram Iqbal Nagra <hnagra.bscs21seecs@seecs.edu.pk>
2024-08-30 23:36:58 +0500

    hi

--- namehole.js
+++ namehole.js
@@ -29,7 +29,7 @@ controls.dampingFactor = 0.03;
 const renderScene = new RenderPass(scene, camera);
 const bloomPass = new UnrealBloomPass(new THREE.Vector2(w, h), 1.5, 0.4, 100);
 bloomPass.threshold = 0.002;
-bloomPass.strength = 1;
+bloomPass.strength = 1.9;
 bloomPass.radius = 0.1;
 const composer = new EffectComposer(renderer);
 composer.addPass(renderScene);
